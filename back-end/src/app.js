338aaa19c3ae0e03b9b3afcb2e06df03c3fd9784
Abdul M Nasir <moeid.nasir@gmail.com>
2023-09-14 16:05:51 -0400

    new

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -8,13 +8,19 @@ const cors = require("cors");
 const errorHandler = require("./errors/errorHandler");
 const notFound = require("./errors/notFound");
 const reservationsRouter = require("./reservations/reservations.router");
+const tablesRouter = require("./tables/tables.router");
 
 const app = express();
 
-app.use(cors());
+const corsOptions = {
+  origin: ['http://localhost:3000', 'https://starter-restaurant-reservation-frontend-ehd6.onrender.com'],
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 
 app.use("/reservations", reservationsRouter);
+app.use("/tables", tablesRouter);
 
 app.use(notFound);
 app.use(errorHandler);
